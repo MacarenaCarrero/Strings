f05661dace75491977cb1b23c7240bc3ec4eecb9
Diegohhallam <macarena.c.rios@gmail.com>
2024-10-18 18:48:25 +0200

    Strings

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+console.log(Math.ceil(4.7));
+console.log(Math.floor(4.7));
+console.log(Math.round(4.7));
+
+console.log(Math.PI);
+
+console.log(Math.random());
+
+console.log(Math.random() * 11);
+
+console.log(Math.floor(Math.random() * 11));
+
+const word1 = "Hola";
+console.log(word1.length);
+
+const word2 = "Holis";
+console.log(word2.charAt(0));
+
+const word3 = "Holita";
+console.log(word3.charAt(word3.length - 1));
+
+const word = "xavier";
+console.log(word.includes("o"));
+console.log(word.includes("x"));
+
+const words = "Hola y adios";
+console.log(words.indexOf("o"));
+
+const word5 = " Hola y Adios";
+console.log(word5.toUpperCase());
+console.log(word5.toLowerCase());
+
+const example = "Otro mas";
+console.log(example.startsWith("otro mas"));
+console.log(example.startsWith("Otro"));
+
+const joder = "Venga";
+console.log(joder.endsWith("a"));
+console.log(joder.endsWith("u"));
+
+const sol = "puuuff";
+console.log(sol.substring(2, 4));
+console.log(sol.substring(2));
+console.log(sol.substring(0, 4));
+console.log(sol.substring(0));
+console.log(sol.substring(0, 1));
